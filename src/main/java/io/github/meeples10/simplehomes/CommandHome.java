7411e7127ba7cbe53396d6ea8d98d55987c5fa9b
Meeples10 <mc.meeples10@gmail.com>
2022-08-02 15:12:35 -0400

    Implement functionality

--- src/main/java/io/github/meeples10/simplehomes/CommandHome.java
+++ src/main/java/io/github/meeples10/simplehomes/CommandHome.java
@@ -3,13 +3,43 @@ package io.github.meeples10.simplehomes;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import net.kyori.adventure.sound.Sound;
 
 public class CommandHome implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+        if(sender instanceof Player) {
+            if(sender.hasPermission("simplehomes.home")) {
+                Player p = (Player) sender;
+                String name = "home";
+                if(args.length > 0) {
+                    name = args[0];
+                }
+                name = name.replaceAll("[^A-Za-z0-9_\\-]", "_");
+                Home home = Main.getHome(p.getUniqueId(), name);
+                if(home != null) {
+                    if(Main.resetVelocity) {
+                        p.setVelocity(new Vector(0, 0, 0));
+                        p.setFallDistance(0.0f);
+                    }
+                    p.teleport(home.location);
+                    if(Main.playSound) {
+                        p.playSound(Main.teleportSound, Sound.Emitter.self());
+                    }
+                    sender.sendMessage(String.format(Main.teleportMessage, name));
+                } else {
+                    sender.sendMessage(Main.homeNotFoundMessage);
+                }
+            } else {
+                sender.sendMessage(Main.noPermissionMessage);
+            }
+        } else {
+            sender.sendMessage(Main.playersOnlyMessage);
+        }
         return true;
     }
-
 }
--- src/main/java/io/github/meeples10/simplehomes/CommandHomes.java
+++ src/main/java/io/github/meeples10/simplehomes/CommandHomes.java
@@ -1,15 +1,44 @@
 package io.github.meeples10.simplehomes;
 
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class CommandHomes implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+        if(sender instanceof Player) {
+            if(sender.hasPermission("simplehomes.homes")) {
+                Player p = (Player) sender;
+                List<Home> homes = Main.HOMES.get(p.getUniqueId());
+                if(homes.size() == 0) {
+                    sender.sendMessage(Main.noHomesMessage);
+                    return true;
+                }
+                StringBuilder sb = new StringBuilder();
+                int i = 0;
+                for(Home home : homes) {
+                    sb.append(ChatColor.DARK_AQUA);
+                    sb.append(home.name);
+                    if(i < homes.size() - 1) {
+                        sb.append(ChatColor.GRAY);
+                        sb.append(", ");
+                    }
+                    i++;
+                }
+                sender.sendMessage(sb.toString());
+            } else {
+                sender.sendMessage(Main.noPermissionMessage);
+            }
+        } else {
+            sender.sendMessage(Main.playersOnlyMessage);
+        }
         return true;
     }
-
 }
